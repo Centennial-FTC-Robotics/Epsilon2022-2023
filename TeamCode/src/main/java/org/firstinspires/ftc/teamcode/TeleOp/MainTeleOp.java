2ce89d604dbab157e683ec42965edf3f5a685be7
Justin Zhou <justzh2016@gmail.com>
2023-01-04 21:48:14 -0500

    Drive formula

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/MainTeleOp.java
@@ -1,7 +1,13 @@
 package org.firstinspires.ftc.teamcode.TeleOp;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.opMode;
+
+import static Epsilon.Subsystems.EpsilonRobot.drivetrain;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 import Epsilon.Subsystems.EpsilonRobot;
 
@@ -15,7 +21,16 @@ public class MainTeleOp extends LinearOpMode {
         waitForStart();
 
         while(opModeIsActive()){
-            //driving code
+
+            double yPower = this.gamepad1.left_stick_y;
+            double xPower = this.gamepad1.left_stick_x;
+            double turn = this.gamepad1.right_stick_x;
+
+            drivetrain.frontLeft.setPower(yPower - xPower + turn);
+            drivetrain.backLeft.setPower(yPower + xPower + turn);
+            drivetrain.frontRight.setPower(yPower + xPower  - turn);
+            drivetrain.backRight.setPower(yPower - xPower - turn);
+
         }
     }
 }
