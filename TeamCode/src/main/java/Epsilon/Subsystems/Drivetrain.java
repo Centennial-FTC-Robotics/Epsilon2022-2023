96b508cf418bbca2546aeaa1b571d06cbcf6eef9
Justin Zhou <justzh2016@gmail.com>
2023-01-10 20:47:23 -0500

    Basic intake and Autonomous Driving methods

--- TeamCode/src/main/java/Epsilon/Subsystems/Drivetrain.java
+++ TeamCode/src/main/java/Epsilon/Subsystems/Drivetrain.java
@@ -29,6 +29,64 @@ public class Drivetrain implements Subsytem {
         frontRight.setPower(yPower + xPower  - turn);
         backRight.setPower(yPower - xPower - turn);
     }
+    public enum MoveType {
+        DRIVE,
+        STRAFE,
+        TURN
+    }
+
+    public void TargetPos(double EncoderCounts, MoveType Type) {
+        switch (Type) {
+            case DRIVE:
+                frontLeft.setTargetPosition((int) EncoderCounts);
+                frontRight.setTargetPosition((int) EncoderCounts);
+                backLeft.setTargetPosition((int) EncoderCounts);
+                backRight.setTargetPosition((int) EncoderCounts);
+                break;
+            case STRAFE:
+                frontLeft.setTargetPosition((int) -EncoderCounts);
+                frontRight.setTargetPosition((int) EncoderCounts);
+                backLeft.setTargetPosition((int) EncoderCounts);
+                backRight.setTargetPosition((int) -EncoderCounts);
+                break;
+            case TURN:
+                frontLeft.setTargetPosition((int) -EncoderCounts);
+                frontRight.setTargetPosition((int) EncoderCounts);
+                backLeft.setTargetPosition((int) -EncoderCounts);
+                backRight.setTargetPosition((int) EncoderCounts);
+                break;
+        }
+        frontLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    public double INtoEC(double inches) {
+        //Inches to Encoder Counts Stuff
+        //(8192 ticks / 1 rev) * (1 rev / 1.96pi )
+        double COUNTS_PER_INCH = 8192/(Math.PI*1.96);
+        double EncoderCounts = inches * COUNTS_PER_INCH;
+        return EncoderCounts;
+    }
+
+    public void Move(double power, int inches, MoveType Type, LinearOpMode opMode) {
+        double EncoderCounts = INtoEC(inches);
+        TargetPos(EncoderCounts, Type);
+        frontLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        frontLeft.setPower(power);
+        frontRight.setPower(power);
+        backLeft.setPower(power);
+        backRight.setPower(power);
+        while (frontLeft.isBusy() || frontRight.isBusy() || backLeft.isBusy()
+                || backRight.isBusy() && opMode.opModeIsActive()) {
+
+        }
+    }
 
     public void initialize(LinearOpMode opMode, EpsilonRobot robot){
         //instantiating the motors on the hardware map
---
+++ TeamCode/src/main/java/Epsilon/Subsystems/Intake.java
@@ -0,0 +1,43 @@
+package Epsilon.Subsystems;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import Epsilon.EpsilonRobot;
+import Epsilon.Subsytem;
+
+public class Intake implements Subsytem {
+
+    public DcMotor fourBar;
+    public Servo claw;
+
+    @Override
+    public void initialize(LinearOpMode opMode, EpsilonRobot robot) {
+        DcMotor fourBar = opMode.hardwareMap.dcMotor.get("fourBar");
+        Servo claw = opMode.hardwareMap.servo.get("claw");
+
+        fourBar.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    }
+
+    public void suckStuff(boolean state){
+        if(state){
+            fourBar.setPower(1);
+        } else {
+            fourBar.setPower(0);
+        }
+    }
+
+    public void grab(){
+        claw.setPosition(1);
+    }
+
+    public void release(){
+        claw.setPosition(0);
+    }
+
+    @Override
+    public boolean active() {
+        return true;
+    }
+}
