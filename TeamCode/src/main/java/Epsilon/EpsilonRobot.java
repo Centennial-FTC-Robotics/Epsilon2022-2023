ccd76c3c2aba54b0678d22fe64a72032aa0c7f13
teky1 <officialteky@gmail.com>
2023-01-10 21:29:20 -0500

    fixed imports

--- TeamCode/src/main/java/Epsilon/EpsilonRobot.java
+++ TeamCode/src/main/java/Epsilon/EpsilonRobot.java
@@ -2,12 +2,16 @@ package Epsilon;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import Epsilon.Subsystems.Drivetrain;
+import Epsilon.Subsystems.Intake;
+import Epsilon.Subsystems.Outtake;
 
 public class EpsilonRobot {
 
     public Drivetrain drivetrain = new Drivetrain();
+    public Intake intake = new Intake();
+    public Outtake outtake = new Outtake();
     public Subsystem[] subsystems = new Subsystem[] {
-      drivetrain
+      drivetrain, intake, outtake
     };
 
     public void initialize(LinearOpMode opMode){
